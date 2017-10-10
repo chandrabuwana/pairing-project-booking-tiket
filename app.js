12445d10ecf79510a9eef0923737aa277c3bc287
= <pangkeyd@gmail.com>
2017-10-10 21:29:28 +0700

    ejsCommit

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
 let express = require('express');
 let bodyParser = require('body-parser');
-let exphbs=require('express-handlebars')
-let nodemailer=require('nodemailer');
+let exphbs = require('express-handlebars')
+let nodemailer = require('nodemailer');
+let path = require('path')
 //--------------------------------------------------------
 let app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, '/public')))
 
 app.set('view engine', 'ejs');
 
+let index = require('./routes/index')
+let city = require('./routes/city')
 
-let city=require('./routes/city')
-
-app.use('/',city)
+app.use('/', index)
+// app.use('/', city)
 
 
 app.listen(3005, function(){
