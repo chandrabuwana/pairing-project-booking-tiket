3058cdc6620aa8b4fcba9886745b5809976c4909
chandrabuwana <cbuwana@gmail.com>
2017-10-11 10:21:14 +0700

    rubah smua

--- routes/city.js
+++ routes/city.js
@@ -19,4 +19,25 @@ router.post('/',(req,res)=>{
     res.send(book)
   })
 })
+
+router.get('/addcostumer/:id',(req,res)=>{
+  model.City.findById(req.params.id)
+  .then((dataCity)=>{
+    model.Costumer.findAll()
+    .then((dataCostumer)=>{
+      res.render('addcostumer',{dataCity:dataCity,dataCostumer:dataCostumer})
+    })
+  })
+})
+
+router.post('/addcostumer/:id',(req,res)=>{
+  model.Costumer.create({
+    CityId:req.params.id,
+    first_name:req.body.first_name,
+    last_name:req.body.last_name,
+    email:req.body.email,
+    no_telp:req.body.no_telp,
+
+  })
+})
 module.exports = router;
